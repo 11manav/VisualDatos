1bb7a77c1610c772a52b127f5695346d0c42d9cf
Aryan Pawar <aryandpawar22@gmail.com>
2023-01-22 23:31:38 +0530

    Added file upload feature

--- VData/home/views.py
+++ VData/home/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 
 
 
 def home(request):
-    return HttpResponse("Welcome to Homepage")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, './index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, './index.html')
