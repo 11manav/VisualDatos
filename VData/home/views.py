5cddeccf5cce02bc8bfbc07a15987aaa17ee1b54
Aryan Pawar <aryandpawar22@gmail.com>
2023-01-24 00:10:45 +0530

    Displaying Dataset

--- VData/home/views.py
+++ VData/home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
-
+from django.conf import settings
+from django.templatetags.static import static
+import os
+import pandas as pd
 # Create your views here.
 
 
@@ -11,8 +14,9 @@ def home(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, './index.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
+        data = pd.read_csv('./media/{}'.format(myfile.name))
+        data_html = data.to_html()
+        context = {'loaded_data': data_html}
+        return render(request, './testTable.html',context)
     return render(request, './index.html')
+
